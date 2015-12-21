01ab5242449199b8d4f6b21c3b082ed60fdb5ae7
Vlad Georgescu <vlad.georgescu@xirrus.com>
2015-12-21 10:59:41 -0800

    adding project

--- javatest/src/vlad/TestMe.java
+++ javatest/src/vlad/TestMe.java
@@ -1,11 +1,16 @@
 package vlad;
 
+import features.Feature;
+
 public class TestMe {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("I'm testing");
-		System.out.println("Adding something");
+		Feature feature = new Feature();
+		feature.setName("Vlad");
+		feature.setCounter(2);
+		System.out.println("Adding feature: " + feature.toString());
 	}
 
 }
