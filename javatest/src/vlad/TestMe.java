0fba9d635a3202470e8fb81b177c6e2ab669da66
Vlad Georgescu <vlad.georgescu@xirrus.com>
2015-12-21 10:45:51 -0800

    testing changes

--- javatest/src/vlad/TestMe.java
+++ javatest/src/vlad/TestMe.java
@@ -5,6 +5,7 @@ public class TestMe {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("I'm testing");
+		System.out.println("Adding something");
 	}
 
 }
