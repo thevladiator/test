855d54388c40d5e8b513d683fb77301830f623a7
thevladiator <vlad@alumni.ucla.edu>
2015-12-21 11:42:31 -0800

    Update Feature.java

--- features/src/features/Feature.java
+++ features/src/features/Feature.java
@@ -18,7 +18,7 @@ public class Feature {
 	}
 	
 	public String toString() {
-		return "My Feature now is this: " + name + " " + counter;
+		return "My Feature now is this right now: " + name + " " + counter;
 	}
 	
 
