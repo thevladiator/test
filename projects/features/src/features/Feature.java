4d28ee7cb6bdbb483304febe66f0f88e25dcfcff
Vlad Georgescu <vlad.georgescu@xirrus.com>
2016-02-05 17:08:25 -0800

    initial commit

---
+++ projects/features/src/features/Feature.java
@@ -0,0 +1,25 @@
+package features;
+
+public class Feature {
+	
+	private String name;
+	private int counter;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getCounter() {
+		return counter;
+	}
+	public void setCounter(int counter) {
+		this.counter = counter;
+	}
+	
+	public String toString() {
+		return "Featuring my changes: " + name + " " + counter;
+	}
+	
+
+}
---
+++ projects/javatest/src/vlad/TestMe.java
@@ -0,0 +1,16 @@
+package vlad;
+
+import features.Feature;
+
+public class TestMe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I'm testing");
+		Feature feature = new Feature();
+		feature.setName("Vlad");
+		feature.setCounter(2);
+		System.out.println("Now: " + feature.toString());
+	}
+
+}
