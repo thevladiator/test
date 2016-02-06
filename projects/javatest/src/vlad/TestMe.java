b2d0ce4ea5cdee4afa9c684c41bcdd04164c2591
thevladiator <vlad@alumni.ucla.edu>
2016-02-05 17:22:03 -0800

    Update TestMe.java

--- projects/javatest/src/vlad/TestMe.java
+++ projects/javatest/src/vlad/TestMe.java
@@ -10,7 +10,7 @@ public class TestMe {
 		Feature feature = new Feature();
 		feature.setName("Vlad");
 		feature.setCounter(2);
-		System.out.println("Now: " + feature.toString());
+		System.out.println("Now is the best time: " + feature.toString());
 	}
 
 }
